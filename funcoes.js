cc7efe8c96ffced1ea754a0892ab220674ae6038
Lucas Cobra <lucashcobra@gmail.com>
2023-11-16 19:15:04 -0300

    Função em Javascript

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+
+function maioroumenor(n) {
+    if (n <= 10) {
+        return 'Menor ou igual a 10'
+    } else {
+        return 'Maior que 10'
+    }
+}
+
+document.write(`O número é ${maioroumenor(10)}`)
+
+// Função que testa se N é menor ou igual a 10, ou maior que 10. 
