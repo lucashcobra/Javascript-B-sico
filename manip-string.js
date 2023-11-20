09419c83f88f5b2b81344a8ff44de747fb580c1a
Lucas Cobra <lucashcobra@gmail.com>
2023-11-20 13:34:32 -0300

    Manipulaçao de string

---
+++ manip-string.js
@@ -0,0 +1,11 @@
+
+        const minhaString = "Seja forte e corajoso!"
+        const numeroDeCaracteres = minhaString.length
+        const numeroDePalavras = minhaString.split(" ").length
+
+        document.write(`${minhaString}<br/>`)
+        document.write(`O número de caracteres é: ${numeroDeCaracteres}<br/>`)
+        document.write(`O núemro de palavras é: ${numeroDePalavras}`)
+        
+
+        //Mostra no documento o número de caracteres e de palavras que tem na string. 
