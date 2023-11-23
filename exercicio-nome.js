d19bcac9b9a1c1b0f84b35562f2720dc303bfe9f
Lucas Cobra <lucashcobra@gmail.com>
2023-11-23 15:10:14 -0300

    exercicio usando nomes JS

---
+++ exercicio-nome.js
@@ -0,0 +1,5 @@
+ // O script faz aparecer um prompt no navegador pedindo pro user digitar o nome e sobrenome dele. 
+// Logo em seguida ele junta o nome e sobrenome e mostra num alerta.
+let nome1 = window.prompt('Digite seu nome:')
+let nome2 = window.prompt('Digite seu sobrenome:')
+window.alert(`O seu nome e sobrenome é: ${nome1} ${nome2}`)
