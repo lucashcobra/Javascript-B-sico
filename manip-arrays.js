d19ac7ef61728bbd7a8aedcb13c73eb4233517d3
Lucas Cobra <lucashcobra@gmail.com>
2023-11-24 13:07:18 -0300

    Manipulação de arrays parte 1

---
+++ manip-arrays.js
@@ -0,0 +1,8 @@
+const frutas = [" Pera " , " Laranja"]
+        frutas.unshift("Mexerica ") // o unshift coloca um novo item/elemento no começo do array. 
+        // frutas.shift() remove o primeiro item/elemento do array.
+        document.write(frutas)
+
+        const frase = "Javascript é fantástico!" 
+        console.log(frase.startsWith("Java")) // mostra no console se a string começa com a palavra que coloquei, respondendo true or false. 
+        console.log(frase.endsWith("!")) // mostra se a string termina com a palavra que coloquei, respondendo true or false.
